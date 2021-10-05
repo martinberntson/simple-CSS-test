058be7241c393f219e46efc86307a40c64433a80
Martin Berntson <70233350+martinberntson@users.noreply.github.com>
2021-10-05 08:46:51 +0200

    Add files via upload

---
+++ JaneDoe.js
@@ -0,0 +1,15 @@
+function style1() { 
+    var yourElement = document.getElementById('stylish');
+    yourElement.setAttribute('href', 'css/style1.css');
+    
+ }
+ function style2() { 
+    var yourElement = document.getElementById('stylish');
+    yourElement.setAttribute('href', 'css/style2.css');
+    }
+ 
+ function style3() { 
+    var yourElement = document.getElementById('stylish');
+    yourElement.setAttribute('href', 'css/style3.css');
+    }
+ 
